dc6064ef360c9be85683f03edb8783615518a8d5
Daniel Trotter <flare84@gmail.com>
2021-09-13 11:56:31 -0600

    built statless widget and added image

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(),
-    ),
-  );
+  runApp(myCard());
+}
+
+class myCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                foregroundImage: AssetImage('images/my_face.jpeg'),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
 }
