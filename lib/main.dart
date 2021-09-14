e1b00cb12d7bd421dcfb3ee58ba6195215fbd1d2
Daniel Trotter <flare84@gmail.com>
2021-09-13 19:10:46 -0600

    added fonts in, still working on implementing them and tel

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,81 @@
 import 'package:flutter/material.dart';
+import 'UrlLauncher';
 
 void main() {
-  runApp(myCard());
+  runApp(MyCard());
 }
 
-class myCard extends StatelessWidget {
+class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                foregroundImage: AssetImage('images/my_face.jpeg'),
-              )
-            ],
-          )
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              foregroundImage: AssetImage('images/my_face.jpeg'),
+            ),
+            Text(
+              'Daniel Trotter',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FRONT-END DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 200.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                TextButton(
+                  onPressed: () {
+                    UrlLauncher.launch("tel://7245046284"),
+                }
+                 child: Text('724-504-6284'),
+
+                )
+              ),
+        ],
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+
+                )
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
